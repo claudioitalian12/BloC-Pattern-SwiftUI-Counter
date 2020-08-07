42968da1d254289c1371bff085b2ab3938772d0a
Claudio Cavalli <dark_claudio@hotmail.it>
2020-08-08 00:06:40 +0200

    Initial Commit

---
+++ BloC-Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BloC-Pattern
+//
+//  Created by claudio cavalli on 08/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
